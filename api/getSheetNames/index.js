bb5e96d99480b7b3554b7ea8669c40585307ca4f
ez2getinn <33991212+ez2getinn@users.noreply.github.com>
2026-01-14 05:39:55 -0500

    Create index.js

---
+++ api/getSheetNames/index.js
@@ -0,0 +1,20 @@
+module.exports = async function (context, req) {
+  context.res = {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+    body: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ],
+  };
+};
