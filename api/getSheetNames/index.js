a887fc48f0e519fffcc685807676e34c8a5f26e9
ez2getinn <33991212+ez2getinn@users.noreply.github.com>
2026-01-14 11:30:47 -0500

    Update index.js

--- api/getSheetNames/index.js
+++ api/getSheetNames/index.js
@@ -1,18 +1,15 @@
-// api/getSheetNames/index.js
-const { graphGet, mustEnv } = require("../_shared/msGraph");
+const { mustEnv, graphGet, resolveSiteAndDrive } = require("../_shared/msGraph");
 
 module.exports = async function (context, req) {
   try {
     const fileId = mustEnv("MS_EXCEL_FILE_ID");
+    const { driveId } = await resolveSiteAndDrive();
 
-    // Get worksheet list from the Excel workbook
     const data = await graphGet(
-      `/me/drive/items/${encodeURIComponent(fileId)}/workbook/worksheets`
+      `/drives/${encodeURIComponent(driveId)}/items/${encodeURIComponent(fileId)}/workbook/worksheets`
     );
 
-    const names = (data.value || [])
-      .map((w) => w?.name)
-      .filter(Boolean);
+    const names = (data.value || []).map((w) => w.name).filter(Boolean);
 
     context.res = {
       status: 200,
@@ -23,10 +20,7 @@ module.exports = async function (context, req) {
     context.res = {
       status: 500,
       headers: { "Content-Type": "application/json" },
-      body: {
-        ok: false,
-        error: err.message || String(err),
-      },
+      body: { ok: false, error: err.message || String(err) },
     };
   }
 };
