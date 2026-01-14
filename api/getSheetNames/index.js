c1d42175550fb9b6ec77b9b53194031e2004e2ca
ez2getinn <33991212+ez2getinn@users.noreply.github.com>
2026-01-14 09:26:21 -0500

    Update index.js

--- api/getSheetNames/index.js
+++ api/getSheetNames/index.js
@@ -2,19 +2,6 @@ module.exports = async function (context, req) {
   context.res = {
     status: 200,
     headers: { "Content-Type": "application/json" },
-    body: [
-      "Jan",
-      "Feb",
-      "Mar",
-      "Apr",
-      "May",
-      "Jun",
-      "Jul",
-      "Aug",
-      "Sep",
-      "Oct",
-      "Nov",
-      "Dec"
-    ],
+    body: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
   };
 };
